3bcd86d2dc9d19228101da7ddeb64e0f73f18aad
Harim Choi <harim1206@gmail.com>
2018-02-08 20:22:56 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,11 @@ def line(line)
   if line.size == 0
     puts ("The line is currently empty.")
   else
+
+    str = ""
+    line.each do |name|
+      str+=
+
     puts("The line is currently: #{string}")
   end
 
