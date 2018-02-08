125b11a7f935424d1135e4bf73529f6b0264973d
Harim Choi <harim1206@gmail.com>
2018-02-08 20:31:05 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,19 +4,15 @@ def line(line)
 
   if line.size == 0
     puts ("The line is currently empty.")
-    
+
   else
 
     i = 0
     str = ""
 
     while i < line.size
-      if i == line.size-1
-        str += "#{i+1}. #{line[i]}"
-      else
-        str += "#{i+1}. #{line[i]} "
-        i += 1
-      end
+      str += "#{i+1}. #{line[i]} "
+      i += 1
     end
 
     puts("The line is currently: #{str}")
